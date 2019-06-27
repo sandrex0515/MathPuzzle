510885dea4ca48de998185ad12eb86ecc19a1915
sandrex cabrales <sandrex.kami@gmail.com>
2019-06-28 04:19:42 +0800

    read me and final test

--- MathPuzzle.py
+++ MathPuzzle.py
@@ -10,16 +10,17 @@ import random #to randomize number
 #t2 = third row second column
 #t3 = third row third column
 
-f1numrow = random.randrange(0, 10)  # first number in first row random number with range from negative 9 to  positive 10
+f1numrow = random.randrange(0, 9)  # first number in first row random number with range from negative 9 to  positive 10
 f3numrow = random.randrange(5,10) - f1numrow  # 3rd number random number from 5 to 10
 
-s1numrow = random.randrange(0, 10)  # Same explanation
+s1numrow = random.randrange(0, 9)  # Same explanation
 s2numrow = random.randrange(5,10) - s1numrow  # Same explanation
 
-t2numrow = random.randrange(0, 10)  # Same explanation
+t2numrow = random.randrange(0, 9)  # Same explanation
 t3numrow = random.randrange(5,10) - t2numrow  # Same explanation
 
 class Puzzle:
+
     print('')
     print('------------------------------------------- WELCOME TO SANDREX MATH PUZZLE ---------------------------------------------------------')
 
@@ -38,26 +39,124 @@ class Puzzle:
 
 
     def myfunc(self):
-
+      if(x.isalpha() and x.isalnum() or y.isalpha() and y.isalnum() or z.isalpha() and z.isalnum()): #i tried, this is the problem when i input alpha and number it got error! sorry it is beyond my capabilities
+          print('')
+          print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Please Input a Valid Number!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+      else:
         if(int(x) + f1numrow + f3numrow != 10): #logic operator to check if answer is greater than 10 for row 1
             ansf = f1numrow + f3numrow - 10 #logic operator to check if answer is right for row 1
             ansf2 = s1numrow + s2numrow - 10 #logic operator to check if answer is right for row 2
             ansf3 = t2numrow + t3numrow - 10 #logic operator to check if answer is right for row 3
-            if(ansf != int(x)): #logic operator to check if answer is not correct to display the answer for row 1
+
+            if(abs(ansf)) != int(x): #logic operator to check if answer is not correct to display the answer for row 1
 
                 print('                                                      ' + str(f1numrow) + ' | ' + str(
                 f3numrow) + ' | ' + str(x) + ' | = ' + str(abs(ansf)) + " - 1st row wrong " )
-            if(ansf2 != int(y)):
+            else:
+                print('                                                      ' + str(f1numrow) + ' | ' + str(
+                    f3numrow) + ' | ' + str(x) + ' | = ' + str(abs(ansf)) + " - 1st row correct  ")
+            if(abs(ansf2)) != int(y):
 
                 print(
                 '                                                      ' + str(s1numrow) + ' | ' + str(y) + ' | ' + str(
-                    s2numrow))
+                    s2numrow) + ' | = ' + str(abs(ansf2)) + ' - 2nd row wrong  ')
+            else:
+                print(
+                    '                                                      ' + str(s1numrow) + ' | ' + str(
+                        y) + ' | ' + str(
+                        s2numrow) + ' | = ' + str(abs(ansf2)) + ' - 2nd row correct  ')
+            if(abs(ansf3)) != int(z):
+                 print('                                                      ' + str(z) + ' | ' + str(t2numrow) + ' | ' + str(
+                     t3numrow) + ' | = ' + str(abs(ansf3)) + ' - 3rd row wrong ')
+            else:
+                print('                                                      ' + str(z) + ' | ' + str(
+                    t2numrow) + ' | ' + str(
+                    t3numrow) + ' | = ' + str(abs(ansf3)) + ' - 3rd row correct ')
+
+
+
 
         elif(int(y) + s1numrow + s2numrow != 10): #logic operator to check if answer is greater than 10 for row 2
-            ansf2 = s1numrow + s2numrow - 10
+            ansf = f1numrow + f3numrow - 10  # logic operator to check if answer is right for row 1
+            ansf2 = s1numrow + s2numrow - 10  # logic operator to check if answer is right for row 2
+            ansf3 = t2numrow + t3numrow - 10  # logic operator to check if answer is right for row 3
 
-            print('                                                      ' + str(s1numrow) + ' | ' + str(y) + ' | ' + str(
-                s2numrow))
+            if (abs(ansf)) != int(
+                    x):  # logic operator to check if answer is not correct to display the answer for row 1
+
+                print('                                                      ' + str(f1numrow) + ' | ' + str(
+                    f3numrow) + ' | ' + str(x) + ' | = ' + str(abs(ansf)) + " - 1st row wrong ")
+            else:
+                print('                                                      ' + str(f1numrow) + ' | ' + str(
+                    f3numrow) + ' | ' + str(x) + ' | = ' + str(abs(ansf)) + " - 1st row correct  ")
+            if (abs(ansf2)) != int(y):# logic operator to check if answer is not correct to display the answer for row 2
+
+                print(
+                    '                                                      ' + str(s1numrow) + ' | ' + str(
+                        y) + ' | ' + str(
+                        s2numrow) + ' | = ' + str(abs(ansf2)) + ' - 2nd row wrong  ')
+            else:
+                print(
+                    '                                                      ' + str(s1numrow) + ' | ' + str(
+                        y) + ' | ' + str(
+                        s2numrow) + ' | = ' + str(abs(ansf2)) + ' - 2nd row correct  ')
+            if (abs(ansf3)) != int(z):# logic operator to check if answer is not correct to display the answer for row 3
+                print('                                                      ' + str(z) + ' | ' + str(
+                    t2numrow) + ' | ' + str(
+                    t3numrow) + ' | = ' + str(abs(ansf3)) + ' - 3rd row wrong ')
+            else:
+                print('                                                      ' + str(z) + ' | ' + str(
+                    t2numrow) + ' | ' + str(
+                    t3numrow) + ' | = ' + str(abs(ansf3)) + ' - 3rd row correct ')
+        elif(int(z) + t2numrow + t3numrow != 10):
+            ansf = f1numrow + f3numrow - 10  # logic operator to check if answer is right for row 1
+            ansf2 = s1numrow + s2numrow - 10  # logic operator to check if answer is right for row 2
+            ansf3 = t2numrow + t3numrow - 10  # logic operator to check if answer is right for row 3
+
+            if (abs(ansf)) != int(
+                    x):  # logic operator to check if answer is not correct to display the answer for row 1
+
+                print('                                                      ' + str(f1numrow) + ' | ' + str(
+                    f3numrow) + ' | ' + str(x) + ' | = ' + str(abs(ansf)) + " - 1st row wrong ")
+            else:
+                print('                                                      ' + str(f1numrow) + ' | ' + str(
+                    f3numrow) + ' | ' + str(x) + ' | = ' + str(abs(ansf)) + " - 1st row correct  ")
+            if (abs(ansf2)) != int(y):# logic operator to check if answer is not correct to display the answer for row 2
+
+                print(
+                    '                                                      ' + str(s1numrow) + ' | ' + str(
+                        y) + ' | ' + str(
+                        s2numrow) + ' | = ' + str(abs(ansf2)) + ' - 2nd row wrong  ')
+            else:
+                print(
+                    '                                                      ' + str(s1numrow) + ' | ' + str(
+                        y) + ' | ' + str(
+                        s2numrow) + ' | = ' + str(abs(ansf2)) + ' - 2nd row correct  ')
+            if (abs(ansf3)) != int(z):# logic operator to check if answer is not correct to display the answer for row 3
+                print('                                                      ' + str(z) + ' | ' + str(
+                    t2numrow) + ' | ' + str(
+                    t3numrow) + ' | = ' + str(abs(ansf3)) + ' - 3rd row wrong ')
+            else:
+                print('                                                      ' + str(z) + ' | ' + str(
+                    t2numrow) + ' | ' + str(
+                    t3numrow) + ' | = ' + str(abs(ansf3)) + ' - 3rd row correct ')
+        else:
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Wow your answer is all correct !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            print("                        #########################################################################################")
+            print('                                                      ' + str(f1numrow) + ' | ' + str(
+                f3numrow) + ' | ' + str(x) + ' | = ' + str(x) + " - 1st row correct  ")
+            print(
+                '                                                      ' + str(s1numrow) + ' | ' + str(
+                    y) + ' | ' + str(
+                    s2numrow) + ' | = ' + str(y) + ' - 2nd row correct  ')
+            print('                                                      ' + str(z) + ' | ' + str(
+                t2numrow) + ' | ' + str(
+                t3numrow) + ' | = ' + str(z) + ' - 3rd row correct ')
+            print("                         #########################################################################################")
+
+
+
+
 
 
 print("------------------------------------------------ Input the answer per row -----------------------------------------------------------")
@@ -70,4 +169,3 @@ p1 = Puzzle(x,y,z)
 
 p1.myfunc()
 
-exit()
--- README.md
+++ README.md
@@ -4,19 +4,23 @@ A simple puzzle console app.
  
 This project is for MaroonStudios Coding Puzzle Exam
 
-#Approach
+# Approach
 
 This is the first time i tried python, i guess this language is easy to learn it is slightly similar to PHP OOP.
 I think an hour on how do i build a puzzle. i have no idea how puzzle works in the first place.
 I searched videos on youtube how do puzzle works i found some math puzzle so i decided that i will automate this puzzle,
 Then i study python in w3school.
 
-#Run in Console
+# Limitation
+
+The application is only a console and the program will not loop. 
+
+# Run in Console
 
 First open this project to IDE. (Im using PyCharm and pyton 3.7).
+if you are not using python v3 kindly change the input to raw_input
 
-
-#Input and output
+# Input and output
 
 Run the MathPuzzle.py and then guess or solve the missing number.
 Input the answer per row:
@@ -27,7 +31,14 @@ Second row answer: (Input your guess or answer number here for the second row)
 
 third row answer: (Input your guess or answer number here for the third row).
 
-#That's all thank you I have fun coding this.
+OUTPUT
+The correct answer will be shown on the 4th row after you input.
+
+
+If you want to get right answer... every row is equal to 10 just minus 
+the two rows and that is the answer. * sample 1 ? 5 = 1 4 5 = 10.
+
+# That's all thank you I had fun coding this.
 
 
 
